adfff4ad88397c2707d1467a0c54c0fc68654100
jaun-van-heerden <39254276+jaun-van-heerden@users.noreply.github.com>
2023-10-12 22:59:20 +1000

    f

--- compare_cspace.py
+++ compare_cspace.py
@@ -1,55 +1,60 @@
+import os
 import numpy as np
-import matplotlib.pyplot as plt
+import cupy as cp
+import pandas as pd
+from scipy.spatial.distance import cosine
 
-def visualize_dim_cspaces(cspace1, cspace2, title1='cspace_seq', title2='cspace_par'):
-    assert cspace1.shape == cspace2.shape, "Both cspaces must have the same shape"
-    assert len(cspace1.shape) >= 2, "Cspace dimensions must be at least 2"
+def exact_equal(arr1, arr2):
+    return cp.all(arr1 == arr2)
 
-    num_dims = len(cspace1.shape)
-    fig, axs = plt.subplots(1, num_dims, figsize=(15, 5))
-    
-    for dim, ax in enumerate(axs):
-        slice_idx1 = [slice(None) if i != dim else 0 for i in range(num_dims)]
-        slice_idx2 = [slice(None) if i != dim else -1 for i in range(num_dims)]
-        
-        slice1 = cspace1[tuple(slice_idx1)]
-        slice2 = cspace2[tuple(slice_idx2)]
-        
-        # Plotting
-        im = ax.imshow(np.concatenate([slice1, np.zeros((slice1.shape[0], 1)), slice2], axis=1), cmap='viridis')
-        ax.set_title(f"Dim {dim}: {title1} vs {title2}")
-        
-        # Add colorbar
-        cbar = plt.colorbar(im, ax=ax)
-        cbar.set_label('Value', rotation=270, labelpad=15)
+def approx_equal(arr1, arr2, tol):
+    return cp.all(cp.isclose(arr1, arr2, atol=tol))
 
-    plt.tight_layout()
-    plt.show()
+def mean_equal(arr1, arr2, tol):
+    return cp.isclose(cp.mean(arr1), cp.mean(arr2), atol=tol)
 
-# Example usage
-cspace_seq = np.load('cspace_gpu.npy')
-cspace_par = np.load('cspace_vec.npy')
+def var_equal(arr1, arr2, tol):
+    return cp.isclose(cp.var(arr1), cp.var(arr2), atol=tol)
 
-print(cspace_seq.shape, cspace_par.shape)
+def cosine_similarity(arr1, arr2):
+    return 1 - cosine(cp.asnumpy(arr1).flatten(), cp.asnumpy(arr2).flatten())
 
+# Initialize DataFrame
+df = pd.DataFrame(columns=['File1', 'File2', 'Metric', 'Tolerance', 'Value'])
+rows_list = []
 
-visualize_dim_cspaces(cspace_seq, cspace_par)
+# Define folder and tolerances
+folder_path = "cspaces"
+tolerances = [0.01, 0.001, 0.0001, 1e-05]
 
+# Read .npy files
+npy_files = [f for f in os.listdir(folder_path) if f.endswith('.npy')]
+arrays_dict = {}
 
+for file in npy_files:
+    full_path = os.path.join(folder_path, file)
+    arrays_dict[file] = cp.load(full_path)
 
+# Compare each pair of files
+for file1, arr1 in arrays_dict.items():
+    for file2, arr2 in arrays_dict.items():
+        if file1 == file2 or arr1.shape != arr2.shape:
+            continue
 
-mean_seq = np.mean(cspace_seq)
-mean_par = np.mean(cspace_par)
+        #rows_list.append({'File1': file1, 'File2': file2, 'Metric': 'Exact Equal', 'Tolerance': 'N/A', 'Value': exact_equal(arr1, arr2).tolist()})
 
-if np.isclose(mean_seq, mean_par, atol=1e-2):
-    print("Means are close.")
-else:
-    print("Means are not close.")
+        for tol in tolerances:
+            rows_list.append({'File1': file1, 'File2': file2, 'Metric': 'Approx Equal', 'Tolerance': tol, 'Value': approx_equal(arr1, arr2, tol).tolist()})
+            rows_list.append({'File1': file1, 'File2': file2, 'Metric': 'Mean Equal', 'Tolerance': tol, 'Value': mean_equal(arr1, arr2, tol).tolist()})
+            rows_list.append({'File1': file1, 'File2': file2, 'Metric': 'Var Equal', 'Tolerance': tol, 'Value': var_equal(arr1, arr2, tol).tolist()})
 
-var_seq = np.var(cspace_seq)
-var_par = np.var(cspace_par)
+        rows_list.append({'File1': file1, 'File2': file2, 'Metric': 'Cosine Similarity', 'Tolerance': 'N/A', 'Value': cosine_similarity(arr1, arr2)})
 
-if np.isclose(var_seq, var_par, atol=1e-2):
-    print("Variances are close.")
-else:
-    print("Variances are not close.")
+# Append rows to DataFrame
+df = pd.concat([df, pd.DataFrame(rows_list)], ignore_index=True)
+
+# Save to CSV
+#df.to_csv('array_comparison_results.csv', index=False)
+
+# Display the DataFrame (useful in Jupyter Notebook)
+print(df)
